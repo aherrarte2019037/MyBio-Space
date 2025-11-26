955d3cd822a14910053f576a3df49412d8ff8e32
Angel <angelherrarte3@gmail.com>
2025-11-26 10:53:41 -0600

    Update project documentation to reflect recent changes in theming and structure
    - Clarified the new theming strategy and its implementation across apps.
    - Added detailed instructions for utilizing the updated project structure.
    - Enhanced guidelines for creating new apps and managing dependencies.

---
+++ apps/template/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./tailwind.css";
+import { ThemeProvider } from "@repo/ui";
+
+export const metadata: Metadata = {
+	title: "Template",
+	description: "Template",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
