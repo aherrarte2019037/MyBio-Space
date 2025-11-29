3327d5b6bb2950bab4e6ca3938f792f237594a6c
Angel <angelherrarte3@gmail.com>
2025-11-29 12:40:38 -0600

    feat: add SQL function `complete_onboarding_step` to track user onboarding progress.

---
+++ packages/db/drizzle/0004_update_complete_onboarding_function.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION complete_onboarding_step(step_name text)
+RETURNS void
+LANGUAGE plpgsql
+SET search_path = public, pg_catalog
+SECURITY DEFINER
+AS $$
+BEGIN
+  UPDATE public.profiles
+  SET onboarding_steps = array_append(onboarding_steps, step_name::public.onboarding_steps)
+  WHERE id = auth.uid()
+  AND NOT (onboarding_steps @> ARRAY[step_name::public.onboarding_steps]);
+END;
+$$;
